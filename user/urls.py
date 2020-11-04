9e082911f3aafc6631f0346d74062d0cc7410a3a
Olaniyi Ajayi <ajayiolaniyi8@gmail.com>
2020-11-04 11:35:57 +0100

    work on site content

--- user/urls.py
+++ user/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from django.contrib.auth import  views as auth_views
 from . import views
 
+app_name = 'user'
+
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='user/login.html'), name='login'),
